df4043ec85d00878d12ebfffb37585de2ca98668
dhananjai chand <dcdevdhan@gmail.com>
2021-11-13 22:01:46 -0800

    enhanced functionality

--- main.py
+++ main.py
@@ -12,4 +12,43 @@ app.secret_key = 'my secret and not your secret'
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/donorAccess', methods=['POST', 'GET'])
+def login():
+    username = request.args.get('email')
+    password = request.args.get('pass')
+    validation(username, password)  #chcek if username and password exists in DB
+    return redirect(url_for('dashboard'), code=307)
+
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/profile', methods=['POST', 'GET'])
+def profile():
+    return render_template('pages-profile.html')
+
+@app.route('/events', methods=['POST', 'GET'])
+def events():
+    return render_template('events.html')
+
+@app.route('/donations', methods=['POST', 'GET'])
+def donations():
+    return render_template('donations.html')
+
+def validation(username, password):
+    pass
+
+@app.route('/addEvent')
+def addEvent():
+    time = request.args.get('time')
+    date = request.args.get('date')
+    venue = request.args.get('venue')
+    #Call db from here to save this data
+
+def cancelEvent():
+    pass
+
+def donate():
+    pass
